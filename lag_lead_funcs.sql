4f1a4c860f847d31dbdc0d73d8e09422a98fa906
Mehmet Ali Kaya <59140382+mehmetalikaya-hub@users.noreply.github.com>
2021-08-11 18:10:36 +0300

    Update lag_lead_funcs.sql

--- lag_lead_funcs.sql
+++ lag_lead_funcs.sql
@@ -12,7 +12,7 @@ FROM Orders
 SELECT P.ProductID,
 O.ShippedDate,
 OD.Quantity,
-LEAD(P.UnitsOnOrder) OVER(ORDER BY O.ShippedDate) S›PAR›S_ADET
+LEAD(P.UnitsOnOrder) OVER(ORDER BY O.ShippedDate) S√ùPAR√ùS_ADET
 FROM Products P
 INNER JOIN [Order Details] OD ON P.ProductID = OD.ProductID
 INNER JOIN Orders O ON OD.OrderID = O.OrderID
@@ -21,27 +21,27 @@ WHERE O.ShippedDate IS NOT NULL;
 
 --4
 SELECT O.OrderDate,
-SUM(OD.Quantity * OD.UnitPrice) AS BUGUNK‹_GEL›R,
-LEAD(SUM(OD.Quantity * OD.UnitPrice)) OVER(ORDER BY O.OrderDate) SONRAK›_G‹N_GEL›R,
-SUM(OD.Quantity * OD.UnitPrice)-LEAD(SUM(OD.Quantity * OD.UnitPrice)) OVER(ORDER BY O.OrderDate) AS GEL›R_FARK
+SUM(OD.Quantity * OD.UnitPrice) AS BUGUNK√ú_GEL√ùR,
+LEAD(SUM(OD.Quantity * OD.UnitPrice)) OVER(ORDER BY O.OrderDate) SONRAK√ù_G√úN_GEL√ùR,
+SUM(OD.Quantity * OD.UnitPrice)-LEAD(SUM(OD.Quantity * OD.UnitPrice)) OVER(ORDER BY O.OrderDate) AS GEL√ùR_FARK
 FROM Orders O
 INNER JOIN [Order Details] OD ON O.OrderID = OD.OrderID
 GROUP BY O.OrderDate
 
 --5
 SELECT O.OrderDate,
-SUM(OD.Quantity * OD.UnitPrice) AS BUGUNK‹_GEL›R,
-LEAD(SUM(OD.Quantity * OD.UnitPrice),7) OVER(ORDER BY O.OrderDate) AS B›R_HAFTA_SONRAK›_G‹N_GEL›R,
-SUM(OD.Quantity * OD.UnitPrice)- LEAD(SUM(OD.Quantity * OD.UnitPrice),7) OVER(ORDER BY O.OrderDate) AS GEL›R_FARK
+SUM(OD.Quantity * OD.UnitPrice) AS BUGUNK√ú_GEL√ùR,
+LEAD(SUM(OD.Quantity * OD.UnitPrice),7) OVER(ORDER BY O.OrderDate) AS B√ùR_HAFTA_SONRAK√ù_G√úN_GEL√ùR,
+SUM(OD.Quantity * OD.UnitPrice)- LEAD(SUM(OD.Quantity * OD.UnitPrice),7) OVER(ORDER BY O.OrderDate) AS GEL√ùR_FARK
 FROM Orders O
 INNER JOIN [Order Details] OD ON O.OrderID = OD.OrderID
 GROUP BY O.OrderDate
 
 --6
 SELECT O.OrderDate,
-SUM(OD.Quantity * OD.UnitPrice) AS BUGUNK‹_GEL›R,
-LEAD(SUM(OD.Quantity * OD.UnitPrice),7,0) OVER(ORDER BY O.OrderDate) AS B›R_HAFTA_SONRAK›_G‹N_GEL›R,
-SUM(OD.Quantity * OD.UnitPrice)- LEAD(SUM(OD.Quantity * OD.UnitPrice),7,0) OVER(ORDER BY O.OrderDate) AS GEL›R_FARK
+SUM(OD.Quantity * OD.UnitPrice) AS BUGUNK√ú_GEL√ùR,
+LEAD(SUM(OD.Quantity * OD.UnitPrice),7,0) OVER(ORDER BY O.OrderDate) AS B√ùR_HAFTA_SONRAK√ù_G√úN_GEL√ùR,
+SUM(OD.Quantity * OD.UnitPrice)- LEAD(SUM(OD.Quantity * OD.UnitPrice),7,0) OVER(ORDER BY O.OrderDate) AS GEL√ùR_FARK
 FROM Orders O
 INNER JOIN [Order Details] OD ON O.OrderID = OD.OrderID
 GROUP BY O.OrderDate
@@ -55,7 +55,7 @@ from Region;
 SELECT P.ProductID,
 O.ShippedDate,
 OD.Quantity,
-LAG(P.UnitsOnOrder,3) OVER(ORDER BY O.ShippedDate) ‹«_G‹N_÷NCEK›_S›PAR›S_ADET
+LAG(P.UnitsOnOrder,3) OVER(ORDER BY O.ShippedDate) √ú√á_G√úN_√ñNCEK√ù_S√ùPAR√ùS_ADET
 FROM Products P
 INNER JOIN [Order Details] OD ON P.ProductID = OD.ProductID
 INNER JOIN Orders O ON OD.OrderID = O.OrderID
@@ -65,10 +65,79 @@ WHERE O.ShippedDate IS NOT NULL;
 SELECT P.ProductID,
 O.ShippedDate,
 OD.Quantity,
-LAG(P.UnitsOnOrder,3,-1) OVER(ORDER BY O.ShippedDate) ‹«_G‹N_÷NCEK›_S›PAR›S_ADET
+LAG(P.UnitsOnOrder,3,-1) OVER(ORDER BY O.ShippedDate) √ú√á_G√úN_√ñNCEK√ù_S√ùPAR√ùS_ADET
 FROM Products P
 INNER JOIN [Order Details] OD ON P.ProductID = OD.ProductID
 INNER JOIN Orders O ON OD.OrderID = O.OrderID
 WHERE O.ShippedDate IS NOT NULL;
 
---
+--10
+SELECT
+OrderID,
+CustomerID,
+FIRST_VALUE(Freight) OVER(ORDER BY OrderDate) AS ILK_DEGER
+FROM
+Orders
+WHERE ShipVia = 1;
+
+--11
+SELECT
+OrderID,
+CustomerID,
+FIRST_VALUE(Freight) OVER(ORDER BY Freight) AS EN_AZ_AGIRLIK
+FROM
+Orders
+WHERE ShipVia = 1;
+
+--12
+SELECT
+OrderID,
+CustomerID,
+ShipCity,
+OrderDate,
+LAST_VALUE(OrderDate) OVER(ORDER BY OrderDate) AS SON_SIPARIS_TARIHI
+FROM Orders
+
+--13
+
+SELECT
+OrderID,
+CustomerID,
+ShipCity,
+OrderDate,
+LAST_VALUE(OrderDate) OVER(
+ORDER BY OrderDate ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS EN_SON_SIPARIS_TARIHI
+FROM Orders
+
+--14
+
+SELECT OD.UnitPrice,
+OD.Quantity,
+LAST_VALUE(OD.Quantity) OVER(ORDER BY O.OrderDate ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) EN_SON_ALINAN_ADET
+FROM 
+[Order Details] OD
+JOIN Orders O ON OD.OrderID = O.OrderID
+
+
+--15
+
+WITH GELIR_TABLO
+AS
+(
+SELECT
+O.OrderDate,
+SUM((OD.Quantity * OD.UnitPrice) * (1-OD.Discount)) AS NET_GELIR
+FROM Orders O
+JOIN 
+[Order Details] OD ON O.OrderID = OD.OrderID
+GROUP BY O.OrderDate
+)
+SELECT TOP 1
+LAST_VALUE(NET_GELIR) OVER(ORDER BY OrderDate ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS EN_YUKSEK_GELIR,
+FIRST_VALUE(NET_GELIR) OVER(ORDER BY OrderDate ) AS EN_DUSUK_GELIR
+FROM 
+GELIR_TABLO
+
+
+ 
+
